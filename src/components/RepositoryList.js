ba7c2c58f83962facba5b8ef27ee95ecb48c70cb
Dawid Sadlik <dawidsadlik@gmail.com>
2023-07-07 15:39:44 +0200

    impove repository list

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,25 +1,25 @@
 import { React } from "react";
 import { useMeOnGitHubContextProvider } from "../context/MeOnGitHubContextProvider";
-import { Card, Container, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 const RepositoryList = () => {
   const { repositories } = useMeOnGitHubContextProvider();
 
   return (
     <>
-      <Container>
-        <Row>
-          {repositories.map((r) => (
-            <Card style={{ width: "20rem" }} className="p-5">
+      <Row>
+        {repositories.map((r) => (
+          <Col>
+            <Card style={{ width: "16rem", margin: 10 }}>
               <Card.Header>{r.name}</Card.Header>
               <Card.Body>
                 <Card.Title>{r.fullname}</Card.Title>
                 <Card.Text>{r.description}</Card.Text>
               </Card.Body>
             </Card>
-          ))}
-        </Row>
-      </Container>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
